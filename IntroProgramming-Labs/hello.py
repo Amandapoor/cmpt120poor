87c23562396d376049e9281a2977ffc9f8cce954
Amandapoor <amanda.poor1@marist.edu>
2019-02-06 20:43:54 -0500

    Added goodbye message to Hello program

--- IntroProgramming-Labs/hello.py
+++ IntroProgramming-Labs/hello.py
@@ -1,7 +1,9 @@
-Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:44:43) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-============== RESTART: /Users/amanda.poor1/Documents/hello.py ==============
-Hello, instructor!
->>> 
+# Introduction to Programming
+# Author: Amanda Poor
+# Date: 6 February 2019
+
+def main():
+    print("Hello, instructor!")
+    print("Good-bye!")
+  
+main()
