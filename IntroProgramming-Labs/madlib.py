fdf7a507a3ba0b2c9b2a47c0423eed530926dbf2
Amandapoor <amanda.poor1@marist.edu>
2019-02-12 11:38:58 -0500

    Solution to Mad Libs exercise.

---
+++ IntroProgramming-Labs/madlib.py
@@ -0,0 +1,11 @@
+# a program to construct an amusing sentences using users input of nouns, verbs, adjectives, and a place
+
+def main():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adjective = input("Enter an adjective: ")
+    place = input("Enter a place: ")
+    print("Take your "+ adjective+" "+ noun +" and "+ verb + " it at the "+ place)
+
+main()
+    
