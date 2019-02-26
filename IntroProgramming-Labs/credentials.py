d0757076c21fb6709117713dd81a39ca2388efa3
Amandapoor <amanda.poor1@marist.edu>
2019-02-26 18:48:43 -0500

    More improvements to enhance the functionality of credentials.py.

--- IntroProgramming-Labs/credentials.py
+++ IntroProgramming-Labs/credentials.py
@@ -3,30 +3,38 @@
 # Author: Amanda Poor
 # Created: 2019-02-25
 
-
-#The list containing the first and last names was accidentally submitted into
-#the first time I committed via git
 def getName():
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
     return [first, last]
-#The function below was submitted the first time I committed via git    
+
 def getUname(name):
     return name[0]+ "." + name[1]
 
-    
-#I accidentally had the getPassword function return the password if it passed
-#the strength test for 8 characters the first time I committed via git. 
-def getPassword(uname):
-    print("Your username is:", uname)
-    password = input("Create a new password: ")
-    while len(password) < 8:
+def getPasswordstrength(password, uname):
+    if len(password) < 8:
         print("Fool of a Took! That password is feeble!")
         print("Make sure your password is at least 8 letters")
         password = input("Create a new password: ")
+    if password.lower() == password:
+        print("Fool of a Took! That password is feeble!")
+        print("Make at least one character, but not all, upper case: ")
+        password = input("Create a new password: ")
+    if password.upper() == password:
+        print("Fool of a Took! That password is feeble!")
+        print("Make at least one character, but not all, lower case: ")
+        password = input("Create a new password: ")
     print("The force is strong in this one...")
+    print("Your username is ", uname)
     print("Your password is: ", password)
     print("Account configured. Your new email address is", uname + "@marist.edu")
+   
+
+    
+def getPassword(uname):
+    password = input("Create a new password: ")
+    getPasswordstrength(password, uname)
+    
 
 def main():
     name = getName()
