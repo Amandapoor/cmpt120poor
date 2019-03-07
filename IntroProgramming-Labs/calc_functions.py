2ac43c4c2f8289da5726b86a15ae56711a50c72b
Amandapoor <amanda.poor1@marist.edu>
2019-03-06 21:37:21 -0500

    Added has functions

--- IntroProgramming-Labs/calc_functions.py
+++ IntroProgramming-Labs/calc_functions.py
@@ -6,6 +6,27 @@ def getEquation(equation):
 
     return equation
 
+def hasProductDivision(equation):
+    
+    
+    if '*' in equation:
+        return True
+
+    elif '/' in equation:
+        return True
+    else:
+        return False
+    
+
+def hasAdditionSubtract(equation):
+    if '+' in equation:
+        return True
+    
+    elif '-' in equation:
+        return True
+    else:
+        return False
+
    
 def main():
     equation = input("Enter an equation (use spaces in between): ").split()
