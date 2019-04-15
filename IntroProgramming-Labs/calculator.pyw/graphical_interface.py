180ff304d1754c46388bdd00cbb3af272909627f
Amandapoor <amanda.poor1@marist.edu>
2019-04-15 16:52:41 -0400

    Added M+, M-, MR, MC and fixed bug with equation fragment

--- IntroProgramming-Labs/calculator.pyw/graphical_interface.py
+++ IntroProgramming-Labs/calculator.pyw/graphical_interface.py
@@ -6,7 +6,7 @@ from calc_functions import *
 
 
 def createCanvas():
-    win = GraphWin("Calculator",500, 620)
+    win = GraphWin("Calculator",600, 620)
     win.setBackground("grey")
     return win
 
@@ -14,19 +14,25 @@ displayEquation=[]
 displayString=""
 
 def getCoords(i):
-    coords = [[75,120],[165,120],[255,120],[345,120],[75,220],[165,220],[255,220],[345,220],[75,320],[165,320],[255,320],[345,320], [75,420],[165,420],[255,420],[345,420],[255,520],[345,520], [75,520]]
+    coords = [[75,120],[165,120],[255,120],[345,120],[75,220],[165,220],
+              [255,220],[345,220],[75,320],[165,320],[255,320],[345,320],
+              [75,420],[165,420],[255,420],[345,420],[255,520],[345,520],
+              [75,520],[435,120],[435,220],[435,320],[435,420]]
     return coords[i][0], coords[i][1]
 
 def getLabel(i):
-    labels = ['7','8','9','/','4','5','6','*','1','2','3','+','+/-','0','.','-','Del','=','Clr']
+    labels = ['7','8','9','/','4','5','6','*','1','2','3'
+              ,'+','+/-','0','.','-','Del','=','Clr','M+','M-','MR','MC']
     return labels[i]
 
 def createButtons(win):
     color = ['pink','pink','pink','lightblue','pink','pink','pink','lightblue','pink',
-             'pink','pink','lightblue','lightblue','pink','lightblue','lightblue','lightblue','lightblue', 'yellow']
-    size = [80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80]
+             'pink','pink','lightblue','lightblue','pink','lightblue'
+             ,'lightblue','lightblue','lightblue', 'yellow','lightgreen','lightgreen',
+             'lightgreen','lightgreen']
+    size = [80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80,80]
 
-    for i in range(19):
+    for i in range(23):
         x, y = getCoords(i)
         
         button = Rectangle(Point(x,y),Point(x+size[i],y+size[i]))
@@ -35,36 +41,42 @@ def createButtons(win):
         label.setSize(22)
         button.draw(win)
         label.draw(win)
+
+
         
 def createDisplay(win):
 
-    rectangle = Rectangle(Point(75,20), Point(425,100)).draw(win)
+    rectangle = Rectangle(Point(75,20), Point(515,100)).draw(win)
     color2 = "white"
     rectangle.setFill(color2)
-    text = Text(Point(300,60)," ")
+    text = Text(Point(400,60)," ")
     text.setSize(22)
     text.draw(win)
 
     return text
     
 def getInput(mouse,display):
-    for i in range(19):
+    for i in range(23):
         x,y = getCoords(i)
         if x<mouse.x < x+80 and y< mouse.y < y+80:
             print(getLabel(i))
             return getLabel(i)
             break
 
-def addEquation(equation,label):
-#    if label in ['+','-','*','/']:
-#        equation.append(label)
+def addEquation(equation,label,memory):
+
     if len(equation) == 0:
-        if label not in ['+','-','*','/', '+/-']:
+        if label == 'MR':
+            equation.append(str(memory))
+        elif label not in ['+','-','*','/', '+/-']:
             equation.append(label)
     elif label in ['+','-','*','/']:
         equation.append(" " + label + " ")
     elif label == '+/-':
         equation[-1] = str(-1 * float(equation[-1]))
+    elif label == 'MR':
+        if equation[-1] in [' + ',' - ',' * ',' / ']:
+            equation.append(str(memory))
     else:
         equation[-1] = equation[-1] + label
     return equation
@@ -79,6 +91,8 @@ def equationString(equation):
 
     
 def main():
+    memory = 0
+    result = 0
     win = createCanvas()
     display = createDisplay(win)
     equation = []
@@ -89,10 +103,34 @@ def main():
         if label == "=":
             print(equation)
             if len(equation)>0:
-                result=getEquation(equationString(equation).split())
-                display.setText(str(result))
-            else:
-                continue
+                try:
+                    result=getEquation(equationString(equation).split())
+                    display.setText(str(result))
+                except:
+                    continue
+        elif label == "M+":
+            print(equation)
+            if len(equation)>0:
+                try:
+                    result=getEquation(equationString(equation).split())
+                    memory = memory + result
+                    display.setText(str(memory))
+                except:
+                    continue
+        elif label == "M-":
+            print(equation)
+            if len(equation)>0:
+                try:
+                    result=getEquation(equationString(equation).split())
+                    memory = memory - result
+                    display.setText(str(memory))
+                except:
+                    continue
+
+        elif label == "MC":
+            print(equation)
+            memory = 0
+        
         elif label == "Del":
             if len(equation)==0:
                 continue
@@ -106,7 +144,7 @@ def main():
         elif label == None:
             continue
         else:
-            equation = addEquation(equation, label)
+            equation = addEquation(equation, label,memory)
             display.setText(equationString(equation))
 
 
