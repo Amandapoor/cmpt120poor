07f72e06ce036cfd1f24ef18544f00fd1be88555
Amandapoor <amanda.poor1@marist.edu>
2019-04-29 18:14:37 -0400

    Final Version

--- IntroProgramming-Labs/RobotStore.py
+++ IntroProgramming-Labs/RobotStore.py
@@ -1,7 +1,6 @@
 class Product:
     
-    def__init__(self, name, price, quantity):
-        
+    def __init__(self, name, price, quantity): 
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -23,14 +22,16 @@ products = [ Product("Ultrasonic range finder", 2.50, 4),
              Product("Lithium polymer battery", 8.99, 8)]
 
 
+    
 def printStock():
     print()
     print("Available Products")
     print("------------------")
-    for i in range(0,len(productNames)):
-        if productQuantities[i] > 0:
-            print(str(i)+")",productNames[i], "$", productPrices[i])
+    for i in range(0,len(products)):
+        if products[i].inStock(1):
+            print(str(i)+")",products[i].name, "$", products[i].price)
     print()
+    
 def main():
     cash = float(input("How much money do you have? $"))
     while cash > 0:
@@ -39,14 +40,14 @@ def main():
         if vals[0] == "quit": break
         prodId = int(vals[0])
         count = int(vals[1])
-        if productQuantities[prodId] >= count:
-            if cash >= productPrices[prodId]:
-                productQuantities[prodId] -= count
-                cash -= productPrices[prodId] * count
-                print("You purchased", count, productNames[prodId]+".")
+        if products[prodId].inStock(count):
+            if cash >= products[prodId].totalCost(count):
+                products[prodId].decStock(count)
+                cash -= products[prodId].totalCost(count)
+                print("You purchased", count, products[prodId].name +".")
                 print("You have $", "{0:.2f}".format(cash), "remaining.")
             else:
                 print("Sorry, you cannot afford that product.")
         else:
-            print("Sorry, we are sold out of", productNames[prodId])
+            print("Sorry, we are sold out of", products[prodId].name)
 main()
